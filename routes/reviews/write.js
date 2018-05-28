f34495d7cd47db30ee4871fe1cf06ad31513045e
KIMSJ <leesd556@gmail.com>
2018-05-28 11:53:36 +0900

    리뷰등록(reviews/write) 기본 query

--- routes/reviews/write.js
+++ routes/reviews/write.js
@@ -1,10 +1,86 @@
 const express = require('express');
 const router = express.Router();
+const async = require('async');
+
+const upload = require('../../config/multer.js');
+const pool = require('../../config/dbPool.js');
+
 
 //테스트 용 입니다.
 router.get('/', (req, res, next) => {
     res.render('index', { title: 'reviews/write' });
     console.log(req.url);
+});
+
+
+router.post('/', function(req, res){
+	let u_idx;
+	let user_id = req.body.user_id;
+	let r_time = req.body.time;
+	let r_desc = req.body.desc;
+	let r_image = req.body.image;
+
+	let taskArray = [
+	// 1. pool에서 connection 하나 가져오기
+		function(callback) {
+      pool.getConnection(function(err, connection) {
+        if (err) {
+          res.status(500).send({
+            message: "Internal Server Error"
+          });
+          callback("pool.getConnection Error : " + err);
+        } else {
+          callback(null, connection);
+        }
+      });
+    },
+
+    // 2. get user_id from user table
+
+    	function(connection, callback){
+    		let getUserIdQuery = "SELECT u_idx FROM user WHERE u_id = ? ";
+    		connection.query(getUserIdQuery, user_id, function(err, result){
+    			if (err) {
+          res.status(500).send({
+            message: "Internal Server Error"
+          });
+          connection.release(); // connection 반환
+          callback("connection.query Error : " + err);
+        } else {
+			u_idx = result[0].u_idx;
+			callback(null, connection, result[0]);
+				}
+      });
+    },
+
+
+    // 3. insert review info
+    function(connection, result, callback){
+    	let insertReviewQuery = "INSERT INTO review (r_time, r_desc, r_image, u_idx) VALUES (?, ?, ?, ?) ";
+    	connection.query(insertReviewQuery, [r_time, r_desc, r_image, u_idx], function(err, result){
+    	if (err) {
+          res.status(500).send({
+              message: "Internal Server Error"
+          });
+          callback("connection.query Error : " + err);
+        } else {
+          res.status(200).send({
+            message: "Success to Review Resgister"
+          });
+          callback(null, "Success to Review Resgister");
+        }
+        connection.release(); // connection 반환
+      });
+    }
+	]
+
+	async.waterfall(taskArray, function(err, result){
+		if(err){
+			console.log(err);
+		} else {
+			console.log(result);
+		}
+	});
 });
 
 
